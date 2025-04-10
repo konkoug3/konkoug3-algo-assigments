efd5308d4c497dbde25aaaf1d58d34c52b38a130
Kougia Konstantina <k.kougia@opap.gr>
2025-04-10 23:34:41 +0300

    τελοσ

--- assignment-2025-1/transposition_graphs.py
+++ assignment-2025-1/transposition_graphs.py
@@ -1,6 +1,7 @@
 import argparse
 from itertools import combinations
 from collections import defaultdict
+from collections import deque
 
 def generate_nodes(s, t):
     n = s + t
@@ -41,7 +42,9 @@ def is_genlex(path, next_node, length):
     return True
 
 def is_homogeneous(a, b, length):
-    return sum(1 for x, y in zip(to_binary(a, length), to_binary(b, length)) if x != y) == 1
+    a_idx = indices(a, length)
+    b_idx = indices(b, length)
+    return len(set(a_idx) ^ set(b_idx)) == 2
 
 def dfs(graph, current, visited, path, all_paths, total_nodes, length):
     visited.add(current)
@@ -49,67 +52,61 @@ def dfs(graph, current, visited, path, all_paths, total_nodes, length):
     if len(path) == total_nodes:
         all_paths.append(list(path))
     else:
-        for neighbor in graph[current]:
-            if neighbor not in visited:
-                if is_homogeneous(current, neighbor, length) and is_genlex(path, neighbor, length):
-                    dfs(graph, neighbor, visited, path, all_paths, total_nodes, length)
+        for neighbor in sorted(graph[current], reverse=True):
+            if neighbor not in visited and is_homogeneous(current, neighbor, length) and is_genlex(path, neighbor, length):
+                dfs(graph, neighbor, visited, path, all_paths, total_nodes, length)
     visited.remove(current)
     path.pop()
+
+def print_graph(graph):
+    for node in sorted(graph.keys(), reverse=True):
+        neighbors = sorted(graph[node], reverse=True)
+        print(f"{node} -> {neighbors}")
 
 def sigma_j(j, s, t):
     bin_j = format(j, f'0{s - 1}b')
     tail = ''.join('-' if bit == '1' else '+' for bit in bin_j)
     return '0' * t + tail
 
-def next_bts(s):
-    results = []
-    for j in range(2 ** (s - 1)):
-        state = list(sigma_j(j, s, t))
-        sequence = []
-        seen = set()
-        while tuple(state) not in seen:
-            seen.add(tuple(state))
-            sequence.append(''.join(state))
-            state = next_ternary(state)
-        results.append(sequence)
-    return results
-
 def next_ternary(s):
-    s = s.copy()
-    i = len(s) - 1
-    while i >= 2:
-        if s[i - 2] == '0' and s[i - 1] == '-' and s[i] == '+':
-            s[i - 2], s[i - 1], s[i] = '-', '+', '0'
-            if i + 1 < len(s) and s[i + 1] in '+-':
-                s[i + 1] = '0'
-            break
-        elif s[i - 2] == '+' and s[i - 1] == '-' and s[i] == '0':
-            s[i - 2], s[i - 1], s[i] = '0', '+', '-'
-            if i + 1 < len(s) and s[i + 1] in '+-':
-                s[i + 1] = '0'
-            break
-        i -= 1
-    return s
+    s = list(s)
+    for i in reversed(range(2, len(s))):
+        if s[i - 2:i + 1] == ['0', '-', '+']:
+            s[i - 2:i + 1] = ['-', '+', '0']
+            for j in range(i + 1, len(s)):
+                if s[j] in '+-':
+                    s[j] = '0'
+                    break
+            return ''.join(s)
+        elif s[i - 2:i + 1] == ['+', '-', '0']:
+            s[i - 2:i + 1] = ['0', '+', '-']
+            for j in range(i + 1, len(s)):
+                if s[j] in '+-':
+                    s[j] = '0'
+                    break
+            return ''.join(s)
+    return None
 
 def bts_to_binary(s):
     return ''.join('1' if c == '0' else '0' for c in s)
 
 def bts_mode(s, t):
-    all_sequences = next_bts(s)
     length = s + t
-    for sequence in all_sequences:
-        binaries = [bts_to_binary(x) for x in sequence]
+    for j in range(2 ** (s - 1)):
+        seq = []
+        state = sigma_j(j, s, t)
+        seen = set()
+        while state and state not in seen:
+            seen.add(state)
+            seq.append(state)
+            state = next_ternary(state)
+        binaries = [bts_to_binary(x) for x in seq]
         ints = [int(x, 2) for x in binaries]
         index_repr = [indices(n, length) for n in ints]
-        print(sequence)
+        print(seq)
         print(binaries)
         print(index_repr)
         print(ints)
-
-def print_graph(graph):
-    for node in sorted(graph.keys(), reverse=True):
-        neighbors = sorted(graph[node], reverse=True)
-        print(f"{node} -> {neighbors}")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -118,24 +115,31 @@ def main():
     parser.add_argument("mode", choices=["graph", "dfs", "bts"])
     parser.add_argument("start", nargs="?", type=int)
     args = parser.parse_args()
-    global t
-    t = args.t
     length = args.s + args.t
 
     if args.mode == "graph":
         graph = build_graph(args.s, args.t)
         print_graph(graph)
+
     elif args.mode == "dfs":
         graph = build_graph(args.s, args.t)
         start_node = args.start if args.start is not None else max(graph.keys())
+        if start_node not in graph:
+            print(f"Ο κόμβος {start_node} δεν υπάρχει στον γράφο.")
+            return
         all_paths = []
         dfs(graph, start_node, set(), [], all_paths, len(graph), length)
-        for path in all_paths:
-            binaries = [to_binary(n, length) for n in path]
-            indexes = [indices(n, length) for n in path]
-            print(binaries)
-            print(indexes)
-            print(path)
+
+        if not all_paths:
+            print("Δεν βρέθηκε διαδρομή.")
+        else:
+            for path in all_paths:
+                binary_path = [to_binary(n, length) for n in path]
+                index_path = [indices(n, length) for n in path]
+                print(binary_path)
+                print(index_path)
+                print(path)
+
     elif args.mode == "bts":
         bts_mode(args.s, args.t)
 
